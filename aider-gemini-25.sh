223c011536dadb31bed4bf6de45b6c62dfed04ad
Torbjørn Ludvigsen <tobben@fastmail.fm>
2025-04-07 17:49:43 +0200

    Updates aider-gemini-25.sh

--- aider-gemini-25.sh
+++ aider-gemini-25.sh
@@ -1,1 +1,1 @@
-aider --model gemini/gemini-2.5-pro-exp-03-25 --weak-model gpt-4o-mini --no-detect-urls --cache-prompts --no-suggest-shell-commands --dark-mode
+aider --model gemini/gemini-2.5-pro-preview-03-25 --weak-model gpt-4o-mini --no-detect-urls --cache-prompts --no-suggest-shell-commands --dark-mode
