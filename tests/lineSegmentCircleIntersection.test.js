ee8980638ca9c32834927455777240487ab8d79e
Torbjørn Ludvigsen <tobben@fastmail.fm>
2025-04-17 10:18:46 +0200

    test: fixes and and adds some test cases

--- tests/lineSegmentCircleIntersection.test.js
+++ tests/lineSegmentCircleIntersection.test.js
@@ -4,13 +4,13 @@ describe('lineSegmentCircleIntersection', () => {
   const center = new Vector2(0, 0);
 
   test('segment completely outside circle', () => {
-    const p1 = new Vector2(0, 0);
-    const p2 = new Vector2(1, 0);
+    const p1 = new Vector2(0, -1.0000001);
+    const p2 = new Vector2(2, -1);
     expect(lineSegmentCircleIntersection(p1, p2, center, 1)).toBe(false);
   });
 
-  test('segment endpoint inside circle', () => {
-    const p1 = new Vector2(0, 0);
+  test('segment endpoint just inside circle', () => {
+    const p1 = new Vector2(0.99999999, 0);
     const p2 = new Vector2(5, 0);
     expect(lineSegmentCircleIntersection(p1, p2, center, 1)).toBe(true);
   });
@@ -38,4 +38,23 @@ describe('lineSegmentCircleIntersection', () => {
     const p2 = new Vector2(2, 0);
     expect(lineSegmentCircleIntersection(p1, p2, center, 1)).toBe(false);
   });
+
+  test('going through zero-radius circle', () => {
+    const p1 = new Vector2(-2, -2);
+    const p2 = new Vector2(2, 2);
+    expect(lineSegmentCircleIntersection(p1, p2, center, 0.0)).toBe(true);
+  });
+
+  test('segment through circle, negative radius', () => {
+    const p1 = new Vector2(-2, 0);
+    const p2 = new Vector2(2, 0);
+    expect(lineSegmentCircleIntersection(p1, p2, center, -1)).toBe(false);
+  });
+
+  test('segment tangent to circle, negative radius', () => {
+    const p1 = new Vector2(1, -1);
+    const p2 = new Vector2(1, 1);
+    expect(lineSegmentCircleIntersection(p1, p2, center, -1)).toBe(false);
+  });
+
 });
