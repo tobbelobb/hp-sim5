945b32cb965c5d769839747011b38e9dbbc7b06f
Torbjørn Ludvigsen <tobben@fastmail.fm>
2025-06-18 20:35:44 +0200

    Fixes npx vite build

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import { resolve } from 'path';
 import fg from 'fast-glob';
 
 export default defineConfig(async () => {
-  const pages = await fg('**/*.html', { ignore: ['node_modules/**'] });
+
+  const pages = await fg('**/*.html', { ignore: ['node_modules/**', 'attic/**'] });
   const inputs = pages.reduce((o, p) => { o[p] = resolve(__dirname, p); return o; }, {});
 
   return {
+    base: '/hp-sim5/',
     build: { rollupOptions: { input: inputs } },
     assetsInclude: ['**/*.usda']
   };
