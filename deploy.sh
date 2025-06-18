a196b700cba037cbce6842b1afc4794bd33bb0ce
Torbjørn Ludvigsen <tobben@fastmail.fm>
2025-06-18 21:08:10 +0200

    Deploy improvements

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,14 @@ set -e
 
 rm -rf dist
 npx vite build
-git checkout deploy_flipper
-git rm -rf .
-cp -r dist/* .
+git worktree add ../deploy-flipper-temp deploy_flipper
+cd ../deploy-flipper-temp
+rm -rf *
+cp -r ../hp-sim5/dist/* .
 touch .nojekyll
 git add .
 git commit -m "Deploy latest Vite build"
 git push
+cd ..
+git worktree remove ../deploy-flipper-temp
 git checkout main
