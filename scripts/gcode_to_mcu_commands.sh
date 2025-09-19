ca0b68a0b086b3a150d2c92a1fbdf2682e7c1bc5
Torbjørn Ludvigsen <tobben@fastmail.fm>
2025-09-19 13:32:50 +0200

    Prepares feature to be able to read .serial files

--- scripts/gcode_to_mcu_commands.sh
+++ scripts/gcode_to_mcu_commands.sh
@@ -12,12 +12,10 @@ BASENAME="$(basename "$GCODE_FILE" .gcode)"
 ~/klippy-env/bin/python ~/repos/klipper/klippy/klippy.py \
   ~/repos/hp-sim5/examples/klipper/slideprinter/printer-slideprinter-linux-mcu.simple.cfg.incl_extruder_and_heatbed \
   -i $GCODE_FILE \
-  -o "${BASENAME}.serial" \
+  -o ~/repos/hp-sim5/public/examples/mcu_commands/"${BASENAME}.serial" \
   -v -d ~/repos/hp-sim5/examples/klipper/linux_mcu/klipper.dict \
 
 ~/klippy-env/bin/python ~/repos/klipper/klippy/parsedump.py \
   ~/repos/hp-sim5/examples/klipper/linux_mcu/klipper.dict \
-  "${BASENAME}.serial" \
+  ~/repos/hp-sim5/public/examples/mcu_commands/"${BASENAME}.serial" \
   > ~/repos/hp-sim5/public/examples/mcu_commands/${BASENAME}.txt
-
-gio trash "${BASENAME}.serial"
