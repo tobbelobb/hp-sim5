cb6de271b984bacc57a4cb8b377bfc0059657fed
Torbjørn Ludvigsen <tobben@fastmail.fm>
2025-10-09 22:56:49 +0200

    Fix hp-sim mobile load by vendoring usda parser

---
+++ hp-sim/vendor/usda-parser/index.js
@@ -0,0 +1,5 @@
+// Vendored copy of @kroxilon/usda-parser/lib/index.js.
+// Provides the parse helpers without requiring a package bundler.
+export { parse, SyntaxError } from './usda-parser.peggy.js';
+export * from "./USDA_File.js";
+//# sourceMappingURL=index.js.map
