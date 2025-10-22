2768357872d5c7046e47011549ca850dac37955c
Torbjørn Ludvigsen <tobben@fastmail.fm>
2025-10-22 09:18:08 +0200

    taught the host object-model shim to answer the Hangprinter-critical queries `inputs[].axesRelative` and `move.axes[].userPosition`

--- RRF/host/include/GCodes/GCodes.h
+++ RRF/host/include/GCodes/GCodes.h
@@ -2,6 +2,8 @@
 
 #ifdef RRF_HOST_BUILD
 
+#include <array>
+#include <cstddef>
 #include <cstdint>
 
 #include <General/StringRef.h>
@@ -32,9 +34,49 @@ public:
 
 	void HandleReply(GCodeBuffer&, GCodeResult, const char*) noexcept {}
 
+	size_t GetNumInputs() const noexcept { return inputAxesRelative.size(); }
+
+	bool GetAxesRelative(size_t index) const noexcept
+	{
+		return (index < inputAxesRelative.size()) ? inputAxesRelative[index] : false;
+	}
+
+	void SetAxesRelative(size_t index, bool value) noexcept
+	{
+		if (index < inputAxesRelative.size())
+		{
+			inputAxesRelative[index] = value;
+		}
+	}
+
+	size_t GetAxisCount() const noexcept { return axisCount; }
+
+	void SetAxisCount(size_t count) noexcept
+	{
+		axisCount = (count <= userPosition.size()) ? count : userPosition.size();
+	}
+
+	float GetUserPosition(size_t axis) const noexcept
+	{
+		return (axis < userPosition.size()) ? userPosition[axis] : 0.0f;
+	}
+
+	void SetUserPosition(size_t axis, float value) noexcept
+	{
+		if (axis < userPosition.size())
+		{
+			userPosition[axis] = value;
+		}
+	}
+
+	void ResetUserPositions() noexcept { userPosition.fill(0.0f); }
+
 private:
 	MachineType machineType{MachineType::fff};
 	const char* axisLetters{"XYZABC"};
+	std::array<bool, 1> inputAxesRelative{{false}};
+	std::array<float, MaxAxes> userPosition{};
+	size_t axisCount{3};
 };
 
 #endif
