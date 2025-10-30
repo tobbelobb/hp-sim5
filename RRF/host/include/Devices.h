cba9ceeee592615feb68cbd590293849420c7e0d
Torbjørn Ludvigsen <tobben@fastmail.fm>
2025-10-30 15:33:04 +0100

    Removes the serial devices on host again

--- RRF/host/include/Devices.h
+++ RRF/host/include/Devices.h
@@ -1,16 +1,16 @@
 #pragma once
 #define SUPPORT_USB		0
 
-#include <AsyncSerial.h>
-#include <USARTClass.h>
-#include <SerialCDC.h>
-
-extern AsyncSerial serialUart1;
-extern USARTClass serialUart2;
-#if defined(DUET3_MB6HC)
-extern AsyncSerial serialWiFi;
-#endif
-extern SerialCDC serialUSB;
+//#include <AsyncSerial.h>
+//#include <USARTClass.h>
+//#include <SerialCDC.h>
+//
+//extern AsyncSerial serialUart1;
+//extern USARTClass serialUart2;
+//#if defined(DUET3_MB6HC)
+//extern AsyncSerial serialWiFi;
+//#endif
+//extern SerialCDC serialUSB;
 
 void DeviceInit() noexcept {};
 void StopAnalogTask() noexcept {};
