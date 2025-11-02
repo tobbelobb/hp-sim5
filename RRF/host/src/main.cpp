954de91cfe88d439080b1d89d73fbfd31305c7bf
Torbjørn Ludvigsen <tobben@fastmail.fm>
2025-11-02 22:16:43 +0100

    Revert "Remove HostForceSimulationMode"
    This reverts commit 94506d4c8d7f89eed698cd40953df21647e9727e.

--- RRF/host/src/main.cpp
+++ RRF/host/src/main.cpp
@@ -651,6 +651,7 @@ int main(int argc, char** argv)
 
 		reprap.Init();
 		reprapInitialised = true;
+		reprap.GetGCodes().HostForceSimulationMode(SimulationMode::normal);
 		HostTiming::Reset();
 
 		bool success = true;
