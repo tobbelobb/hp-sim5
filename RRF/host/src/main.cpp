210f4d9d49bcc8de78973302e56d8127dbe88612
Torbjørn Ludvigsen <tobben@fastmail.fm>
2025-11-13 23:36:49 +0100

    Revert "codex' desperate attempt at determinism"
    This reverts commit 3cd599b03a1a0cd656deaee4d956ceee770b2621.

--- RRF/host/src/main.cpp
+++ RRF/host/src/main.cpp
@@ -620,8 +620,6 @@ bool WaitForPrintCompletion() noexcept
             return false;
         }
 
-        std::this_thread::yield();
-
         if (std::chrono::steady_clock::now() - start > kPrintTimeout)
         {
             std::cerr << "Timed out waiting for print to finish\n";
