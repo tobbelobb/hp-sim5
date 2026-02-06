e11b89bc44be8c054f6edcf876e0e924546b5a75
Torbjørn Ludvigsen <tobben@fastmail.fm>
2026-02-06 13:08:22 +0100

    3D PBDVelocityUpdateSystem, PrevFinalPosSystem

--- src/js/cable_joints_3d/commonSystems.js
+++ src/js/cable_joints_3d/commonSystems.js
@@ -1,4 +1,10 @@
-import { VelocityComponent, GravityAffectedComponent, PositionComponent } from './ecs.js';
+import {
+  VelocityComponent,
+  GravityAffectedComponent,
+  PositionComponent,
+  PrevFinalPosComponent,
+  MassComponent
+} from './ecs.js';
 
 export class GravitySystem {
   runInPause = false;
@@ -27,6 +33,42 @@ export class MovementSystem {
       const posComp = world.getComponent(entityId, PositionComponent);
       const velComp = world.getComponent(entityId, VelocityComponent);
       posComp.pos.add(velComp.vel, dt);
+    }
+  }
+}
+
+export class PrevFinalPosSystem {
+  runInPause = false;
+  update(world, dt) {
+    const entities = world.query([PositionComponent, PrevFinalPosComponent]);
+    for (const entityId of entities) {
+      const posComp = world.getComponent(entityId, PositionComponent);
+      const prevFinalPosComp = world.getComponent(entityId, PrevFinalPosComponent);
+      prevFinalPosComp.pos.set(posComp.pos);
+    }
+  }
+}
+
+export class PBDVelocityUpdateSystem {
+  runInPause = false;
+  update(world, dt) {
+    const grabbed = world.getResource('grabbedBall');
+    const entities = world.query([PositionComponent, VelocityComponent, PrevFinalPosComponent, MassComponent]);
+    const epsilon = 1e-9;
+
+    if (dt <= epsilon) return;
+
+    for (const entityId of entities) {
+      if (entityId === grabbed) continue;
+
+      const massComp = world.getComponent(entityId, MassComponent);
+      if (massComp && massComp.mass <= 0) continue;
+
+      const posComp = world.getComponent(entityId, PositionComponent);
+      const velComp = world.getComponent(entityId, VelocityComponent);
+      const prevFinalPosComp = world.getComponent(entityId, PrevFinalPosComponent);
+
+      velComp.vel.subtractVectors(posComp.pos, prevFinalPosComp.pos).scale(1.0 / dt);
     }
   }
 }
