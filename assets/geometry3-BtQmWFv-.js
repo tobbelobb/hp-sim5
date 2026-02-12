d5b5c4c81b675d1eb7ee9142c576b573d75ef661
Torbjørn Ludvigsen <tobben@fastmail.fm>
2026-02-12 13:10:56 +0100

    Deploy Vite build from 1b13f50 on 2026-02-12 13:10:56

--- assets/geometry3-BtQmWFv-.js
+++ assets/geometry3-BtQmWFv-.js
@@ -1,4 +1,4 @@
-import{_ as ul,s as dl,V as Xr,m as fl,r as pl}from"./cable_joints_core-DZwJGTUI.js";/**
+import{_ as ul,s as dl,V as Xr,m as fl,r as pl}from"./cable_joints_core-BzOaE8bc.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
