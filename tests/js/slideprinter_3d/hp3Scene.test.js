b8f9a0cb48f99d90047da412d9e8cd22da65041d
Torbjørn Ludvigsen <tobben@fastmail.fm>
2026-03-11 11:30:59 +0100

    Fixes a test

--- tests/js/slideprinter_3d/hp3Scene.test.js
+++ tests/js/slideprinter_3d/hp3Scene.test.js
@@ -102,8 +102,8 @@ describe('hp3 USDA scene loading', () => {
     extruderSystem.update(world, 0);
 
     expect(extruder.centerSources.default).toHaveLength(3);
-    expect(extruder.centerPos.z).toBeCloseTo(0.1, 6);
-    expect(extruder.centerOffsets.default.z).toBeCloseTo(0.0, 6);
+    expect(extruder.centerPos.z).toBeCloseTo(0.0, 6);
+    expect(extruder.centerOffsets.default.z).toBeCloseTo(-0.1, 6);
   });
 
   test('marks the hp3 attachment bodies with positive mass as gravity affected', () => {
