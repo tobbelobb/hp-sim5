b05eec723ee90b9b62c278fcbf472c4224c582f4
Torbjørn Ludvigsen <tobben@fastmail.fm>
2026-05-12 15:08:13 +0200

    Deploy Vite build from bdb9981e on 2026-05-12 15:08:13

---
+++ assets/cable_scene_loader-DXsRCHYQ.js
@@ -0,0 +1,1 @@
+import{r as s,O as c}from"./stage-CKWfrQ3w.js";import{b as n}from"./cable_scene_baker-DNZKmHmQ.js";async function b(e,a={}){const r=await s(e),o=n(r,a);return c(o.source)}export{b as O};
